9a5945ace82d5eb74dbc8ae2e0af45cc57d8a421
Peter <peter@graphite.(none)>
2012-07-08 19:22:26 +0200

    Changed to sock file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,6 @@
-listen 5001 # by default Unicorn listens on port 8080
+#listen 5001 # by default Unicorn listens on port 8080
+listen "/var/www/graphiti/shared/unicorn.sock", :backlog => 64
 worker_processes 2 # this should be >= nr_cpus
-pid "/var/sites/graphiti/shared/pids/unicorn.pid"
-stderr_path "/var/sites/graphiti/shared/log/unicorn.log"
-stdout_path "/var/sites/graphiti/shared/log/unicorn.log"
+pid "/var/www/graphiti/shared/pids/unicorn.pid"
+stderr_path "/var/www/graphiti/shared/log/unicorn.log"
+stdout_path "/var/www/graphiti/shared/log/unicorn.log"
